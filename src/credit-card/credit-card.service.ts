1a9cddc4bdba0f9cf4c89f8d7203e7790bfc48a1
melkorAMP <melkor@aleph.engineering>
2024-12-07 01:34:05 -0300

    #4 Update credit card service

--- src/credit-card/credit-card.service.ts
+++ src/credit-card/credit-card.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CreditCardService {}
+export class CreditCardService {
+  validateCreditCardNumber(cardNumber: string): boolean {
+    const sanitizedCardNumber = cardNumber.replace(/\D/g, '');
+
+    // Return false if the sanitized string is empty
+    if (sanitizedCardNumber.length === 0) {
+      return false;
+    }
+
+    let sum = 0;
+    let shouldDouble = false;
+
+    for (let i = sanitizedCardNumber.length - 1; i >= 0; i--) {
+      let digit = parseInt(sanitizedCardNumber.charAt(i), 10);
+
+      if (shouldDouble) {
+        digit *= 2;
+        if (digit > 9) {
+          digit -= 9;
+        }
+      }
+
+      sum += digit;
+      shouldDouble = !shouldDouble;
+    }
+
+    return sum % 10 === 0;
+  }
+}
