6ee3e32662027d1aa90d9dc20656965ebc41ecdc
melkorAMP <melkor@aleph.engineering>
2024-12-07 01:36:15 -0300

    #6 Implement controller after tests success

---
+++ src/credit-card/credit-card.controller.ts
@@ -0,0 +1,30 @@
+import {
+  Controller,
+  Post,
+  Body,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { CreditCardService } from './credit-card.service';
+import { ValidateCreditCardDto } from './dto/validate-credit-card.dto/validate-credit-card.dto';
+
+@Controller('credit-card')
+export class CreditCardController {
+  constructor(private readonly creditCardService: CreditCardService) {}
+
+  @Post('validate')
+  validateCard(@Body() validateCreditCardDto: ValidateCreditCardDto) {
+    const { cardNumber } = validateCreditCardDto;
+
+    const isValid = this.creditCardService.validateCreditCardNumber(cardNumber);
+
+    if (!isValid) {
+      throw new HttpException(
+        'Invalid credit card number',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    return { message: 'Credit card number is valid' };
+  }
+}
